5f75bc6bb7a38967ea82d005518714347fef3400
Patrick Taylor <taylor.patrick@charterschool.org>
2018-02-26 23:07:35 -0500

    matrix get/set works!

--- Matrix.java
+++ Matrix.java
@@ -42,46 +42,54 @@ public class Matrix
         } else {
             if (value != 0){
                 //insert value into A
-                NNZ++;
-                for(int i = row+1; i < IA.length; i++){
-                    IA[i]++;
-                }
-                int lowerBound = IA[row]; //not included
-                int upperBound = IA[row + 1] + 1; //included
-
+                int lowerBound = IA[row]; 
+                //System.out.println(lowerBound);
+                int upperBound = IA[row + 1]; 
+                //System.out.println(upperBound);
                 if(JA.size() != 0){
                     for(int i = lowerBound; i < upperBound; i++){
+                        //System.out.println("something happened");
                         if(JA.get(i) > column){
-                            A.add(i-1,value);
+                            A.add(i,value);
+                            JA.add(i, column);
+                            return;
                         }
                     } 
+                    A.add(upperBound, value);
+                    JA.add(upperBound, column);
                 }else{
                     A.add(value);
                     JA.add(column);
 
                 }
+                NNZ++;
+                for(int i = row+1; i < IA.length; i++){
+                    IA[i]++;
+                }
+                
             }
 
         }
     }
 
     public double get(int row, int column){ //3,4
-        ArrayList<Integer> IAchecks = new ArrayList<Integer>();
+        ArrayList<Integer> IAchecks = new ArrayList<Integer>(); //will contain all indices in A that contain values in row (row)
         int endA = IA[row+1] - 1; //IA[4] - 1, 8 - 1, 7(furthest index to check in A)
-        int diffA = endA - IA[row] + 1; //7 - IA[3] + 1 , 7 - 7 + 1,  (number of elements in row 2)
-        int indexOfReturn =0;
-        for (int i = 0; i < diffA; i++){ //for number of elements in row 2 (diffA)
+        int diffA = (endA - IA[row]) + 1; //7 - IA[3] + 1 , 7 - 7 + 1,  1(number of elements in row (row))
+        int indexOfReturn = 0;
+        for (int i = 0; i < diffA; i++){ //for number of elements in row (row) (diffA)
             IAchecks.add(endA - i); //add to an ArrayList the relevant indices of A (7)
         }
+        if(IAchecks.isEmpty()){return 0;}
         for(int item: IAchecks){
             if(item < JA.size()){
                 if(JA.get(item) == column){
                     indexOfReturn = item;
+                    return A.get(indexOfReturn);
                 }
             } else {return 0;}
         }
-        if(IAchecks.isEmpty()){return 0;}
-        return A.get(indexOfReturn);
+        return 0;
     }
 
     public int getPlace(int row, int column){
@@ -101,6 +109,7 @@ public class Matrix
         }
         return indexOfReturn;
     }
+
     /*
     public static Matrix add(Matrix first, Matrix second){
 
@@ -121,11 +130,25 @@ public class Matrix
     public static Matrix transpose(Matrix matrix){
 
     }
-
-    public static void printMatrix(Matrix matrix){
-
-    }
      */
+    public void printMatrix(){
+        System.out.print("A: ");
+        for (double item : A){
+            System.out.print(item + " ");
+        }
+        System.out.println("");
+        System.out.print("IA: ");
+        for (int item : IA){
+            System.out.print(item + " ");
+        }
+        System.out.println("");
+        System.out.print("JA: ");
+        for (int item : JA){
+            System.out.print(item + " ");
+        }
+        System.out.println("");
+    }
+
     //A  = [ 10 20 30 40 50 60 70 80 ]
     //IA = [  0  2  4  7  8 ]
     //JA = [  0  1  1  3  2  3  4  5 ]  
--- MatrixTest.java
+++ MatrixTest.java
@@ -11,7 +11,11 @@ public class MatrixTest
        Matrix test = new Matrix(3,4);
        test.set(0,0,1);
        test.set(0,1,2);
-       System.out.println(test.get(0,0));
-       System.out.println(test.get(0,1));
+       test.set(0,0,0);
+       test.set(0,1,6);
+       //test.get(0,1);
+       test.printMatrix();
+       //System.out.println(test.get(0,0));
+       //System.out.println(test.get(0,1));
     }
 }
